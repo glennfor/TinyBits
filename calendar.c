399b8b603ba8da47dbaba16ca2fbfdcd43b81186
Glenn-Po <86170577+Glenn-Po@users.noreply.github.com>
2021-11-25 01:40:52 +0100

    Update calendar.c

--- calendar.c
+++ calendar.c
@@ -1,26 +1,28 @@
 
 #include <stdio.h>
 
-
 int main(){
-	//months and their days
+	
 	int year;
-	printf("==PSEUDO CALENDAR====\n\n");
+	printf("|=|PSEUDO CALENDAR|=|\n\n");
 	
 	do{
 		printf("Enter a year[range 0-32000]: ");
 		scanf("%d", &year);
 	}while(year<0 || year > 32000);
 	
-	int isLeapYear = (year%100!=0 && year%4==0)||((year%100==0 && year%4==0) && year%400!=0);
+	int isLeapYear = ((year%100!=0 && year%4==0)||((year%100==0 && year%4==0) && year%400!=0));
 	
+	//index wise days per month: 0-jan, 1-feb, ..., 11-dec
 	int monthDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30,31};
+	
+	//set days in feb  to 29 if leap years
 	if(isLeapYear)monthDays[1] = 29;
 	
 	char* months[] = {"January", "February", "March", "April",
-					"May", "June", "July", "August",
-					"September", "October", "November", "December"
-					};
+			  "May", "June", "July", "August",
+			   "September", "October", "November", "December"
+			 };
 					
 	char overhead[] =
 	 "\nSun Mon Teu Wed Thu Fri Sat\n"
@@ -50,8 +52,5 @@ int main(){
 				printf("\n");
 			}
 		}
-		
-	}
-					
-					
+	}					
 }
